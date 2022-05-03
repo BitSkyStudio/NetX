5671e425437f33f3b74e42f27783c9e6e5d4fb41
mmm <aaa123@pobox.sk>
2022-05-03 16:21:52 +0200

    should now compile with java 8

--- src/main/java/com/github/industrialcraft/netx/timeout/TimeOutHandler.java
+++ src/main/java/com/github/industrialcraft/netx/timeout/TimeOutHandler.java
@@ -8,7 +8,8 @@ import io.netty.handler.timeout.IdleStateEvent;
 public class TimeOutHandler extends ChannelDuplexHandler {
     @Override
     public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
-        if(evt instanceof IdleStateEvent event){
+        if(evt instanceof IdleStateEvent){
+        	IdleStateEvent event = (IdleStateEvent) evt;
             if(event.state() == IdleState.READER_IDLE){
                 ctx.fireExceptionCaught(new TimeoutException("reader idle"));
             } else if(event.state() == IdleState.WRITER_IDLE){
